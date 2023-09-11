a57c7e4c6fc6d7d635feb55a911492095ac3478e
Benedikt Spies <benedikt.spies@gmail.com>
2023-09-11 14:08:21 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"net"
+)
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:0")
+	if err != nil {
+		panic(err)
+	}
+	udpConn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		panic(err)
+	}
+	rawConn, err := udpConn.SyscallConn()
+	if err != nil {
+		panic(err)
+	}
+	var serr error
+	if err := rawConn.Control(func(fd uintptr) {
+		_, serr = unix.GetsockoptInt(int(fd), unix.IPPROTO_UDP, unix.UDP_SEGMENT)
+	}); err != nil {
+		panic(fmt.Errorf("GSO is not supported: %s", err.Error()))
+	}
+	if serr != nil {
+		panic(fmt.Errorf("GSO is not supported: %s", err.Error()))
+	}
+	fmt.Printf("GSO is supported\n")
+}
